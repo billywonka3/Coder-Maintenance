19e197e77c0a57946c7fb75c473ff98835514301
Billy Wong <billywonka3@gmail.com>
2019-07-17 02:38:16 -0400

    Add files via upload

--- routes/bodyRoutes.js
+++ routes/bodyRoutes.js
@@ -13,6 +13,39 @@ router.get("/main", ensureLogin.ensureLoggedIn(), (req, res) => {
   res.render("body/index", { user: req.user });
 });
 
+// router.get('/', (req, res, next) => {
+//   res.render('index');
+// });
+
+// Body Routes
+router.get("/neck-lean", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("body/neck-lean", { user: req.user });
+});
+
+router.get("/neck-asym", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("body/neck-asym", { user: req.user });
+});
+
+router.get("/shoulders", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("body/shoulders", { user: req.user });
+});
+
+router.get("/back-lean", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("body/lowerback-lean", { user: req.user });
+});
+
+router.get("/back-rnd", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("body/lowerback-round", { user: req.user });
+});
+
+router.get("/back-asym", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("body/lowerback-asym", { user: req.user });
+});
+
+router.get("/wrists", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("body/wrists", { user: req.user });
+});
+
 
 // Accessing the API Database via Axios
 // router.get("/main", (req, res, next) => {
