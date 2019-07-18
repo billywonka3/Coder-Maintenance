72530cf26998f7336ce060daa749abe86e69356d
Billy Wong <billywonka3@gmail.com>
2019-07-18 14:25:05 -0400

    Add files via upload

---
+++ models/custom.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+mongoose.plugin(schema => { schema.options.usePushEach = true; }); // this is used when you have an array of object id's that you will be pushing into.
+const Schema   = mongoose.Schema;
+
+
+const customSchema = new Schema({
+  image: {
+    type: String
+  },
+  title: {
+    type: String
+  },
+  desciption: {
+    type: String
+  }
+}, { timestamps: true });
+
+const Body = mongoose.model('Custom', customSchema);
+module.exports = Custom;
