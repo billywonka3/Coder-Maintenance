298a57f46b73fb9b4e89fc2255e16e26584d8d13
Billy Wong <billywonka3@gmail.com>
2019-07-18 15:08:44 -0400

    Add files via upload

--- config/cloundinary-setup.js
+++ config/cloundinary-setup.js
@@ -2,7 +2,6 @@ const cloudinary = require('cloudinary');
 const cloudinaryStorage = require('multer-storage-cloudinary');
 const multer = require('multer');
 
-
 cloudinary.config({
   cloud_name: process.env.CLOUDNAME,
   api_key: process.env.CLOUDKEY,
@@ -11,7 +10,7 @@ cloudinary.config({
 
 var storage = cloudinaryStorage({
   cloudinary: cloudinary,
-  folder: 'ironhack', // The name of the folder in cloudinary
+  folder: 'custom-routines', // The name of the folder in cloudinary
   allowedFormats: ['jpg', 'png'],
   filename: function (req, file, cb) {
     cb(null, file.originalname); // The file on cloudinary would have the same name as the original file name
